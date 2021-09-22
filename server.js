bfeab1df2ee55b0b4a9c31b5044e9085ec2027b5
Pedro Alonso <pedro.paf@gmail.com>
2021-09-22 13:36:48 +0100

    added healthcheck and worker

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
+  .get('/ping', (req, res) => res.send('pong'))
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
